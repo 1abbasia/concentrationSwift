c21db2bd90073fea59c8c15fa579e74524567ad1
Ahsan Abbasi <1abbasia@gmail.com>
2022-08-03 00:44:13 -0400

    button working ish

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBAction func tapToFlipCard(_ sender: UIButton) {
+
+        flipCard(withEmoji: "👻", on: sender)
     }
-
-
 }
 
+func flipCard (withEmoji emoji: String, on button: UIButton){
+    if(button.currentTitle == emoji){
+        button.setTitle("", for: UIControl.State.normal)
+        button.backgroundColor = #colorLiteral(red: 1, green: 0.2527923882, blue: 1, alpha: 1)
+    } else {
+        button.setTitle(emoji, for: UIControl.State.normal)
+        button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+}
