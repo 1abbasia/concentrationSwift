d861e8994f3fd8222da480a4f1a136631830c2a5
Ahsan Abbasi <1abbasia@gmail.com>
2022-08-04 18:54:36 -0400

    fixed button nil issue

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func Card(_ sender: UIButton) {
-    }
+    var flipCount = 0
+    var emoji = "👻"
     
-    @IBAction func tapToFlipCard(_ sender: UIButton) {
-
+    
+    
+    @IBAction func flipFirstCard(_ sender: UIButton) {
         flipCard(withEmoji: "👻", on: sender)
     }
-}
+    
+    
+    @IBAction func flipSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
 
-func flipCard (withEmoji emoji: String, on button: UIButton){
-    if(button.currentTitle == emoji){
-        button.setTitle("", for: UIControl.State.normal)
-        button.backgroundColor = #colorLiteral(red: 1, green: 0.2527923882, blue: 1, alpha: 1)
-    } else {
-        button.setTitle(emoji, for: UIControl.State.normal)
-        button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    func flipCard (withEmoji emoji: String, on button: UIButton){
+        
+        print(button.title(for: UIControl.State.normal))
+        if(button.currentTitle == "👻"){
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .black
+        } else {
+            button.setTitle("👻", for: .normal)
+            button.backgroundColor = .orange
+        }
+        
+        
+        
+        
+//        if(button.currentTitle == emoji){
+//            print(button.currentTitle)
+//            button.setTitle("", for: UIControl.State.normal)
+//            button.backgroundColor = #colorLiteral(red: 1, green: 0.2527923882, blue: 1, alpha: 1)
+//        } else {
+//            button.setTitle(emoji, for: UIControl.State.normal)
+//            button.backgroundColor = .systemOrange    }
     }
 }
